a650e5e0584d83af90155fa4b02ac23f51e12cf1
Renz Bautista <renz.l.bautista@accenture.com>
2026-05-16 04:57:13 +0000

    Exercise 4 Json Binding Creation

--- exercise4/webapp/controller/MainView.controller.js
+++ exercise4/webapp/controller/MainView.controller.js
@@ -1,10 +1,45 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "com/acn/exercise4/model/formatter",
+    "sap/ui/model/json/JSONModel"
+], (Controller, formatter, JSONModel) => {
     "use strict";
 
-    return Controller.extend("com.acn.exercise4.controller.MainView", {
+    return Controller.extend("com.acn.exercise4.controller.JSONBinding", {
+
+        formatter: formatter,
+
         onInit() {
-        }
+
+            var oData = {
+                Eid: "renz.l.bautista",
+                Enabled: true,
+                Address: {
+                    Street: "Zone 8, Brgy. Sampaguita",
+                    City: "Batangas",
+                    Zip: "PH",
+                    Country: "Philippines"
+                },
+                SalesAmount: 999999,
+                CurrencyCode: "Php"
+            };
+
+            var oModel = new JSONModel(oData);
+            this.getView().setModel(oModel);            
+        },
+
+    onSelectionChange: function (oEvent) {
+
+        var oSelectedItem = oEvent.getParameter("listItem");
+        var oContext = oSelectedItem.getBindingContext("ProductsModel");
+        var oSelectedData = oContext.getObject();
+
+        this.getView().setModel(
+            new sap.ui.model.json.JSONModel(oSelectedData),
+            "SelectedProduct"
+        );
+
+    }
+
     });
 });
